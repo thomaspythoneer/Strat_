1425217f351a9edf598bb0413eb70af9a882e40c
Thomas Sajeev Varghese <thomas24101@iiitnr.edu.in>
2025-08-20 19:50:48 +0530

    Create strat.ts

---
+++ tests/strat.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+
+const accounts = simnet.getAccounts();
+const address1 = accounts.get("wallet_1")!;
+
+
+describe("example tests", () => {
+  it("ensures simnet is well initialised", () => {
+    expect(simnet.blockHeight).toBeDefined();
+  });
+
+
+});
